6038de920611c02ca413f160188f6fc09afa7bd0
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2022-01-24 14:12:09 +0900

    finish

--- C06/ex00/ft_print_program_name.c
+++ C06/ex00/ft_print_program_name.c
@@ -6,7 +6,7 @@
 /*   By: hanjung <hanjung@student.42seoul.k>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/22 17:01:20 by hanjung           #+#    #+#             */
-/*   Updated: 2022/01/22 18:47:03 by hanjung          ###   ########.fr       */
+/*   Updated: 2022/01/24 11:27:51 by hanjung          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,9 @@ int	main(int argc, char **argv)
 	int	i;
 
 	i = 0;
-	if (argc == 1)
-		while (argv[0][i])
-			write(1, &argv[0][i++], 1);
+	(void)argc;
+	while (argv[0][i])
+		write(1, &argv[0][i++], 1);
+	write(1, "\n", 1);
 	return (0);
 }
