28f0324e827a7341dd8bd84235d89cf5847569e9
Jung-hansu <97068614+Jung-hansu@users.noreply.github.com>
2022-01-27 13:20:13 +0900

    finish

--- C09/ex02/ft_split.c
+++ C09/ex02/ft_split.c
@@ -6,94 +6,71 @@
 /*   By: hanjung <hanjung@student.42seoul.k>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/26 09:25:57 by hanjung           #+#    #+#             */
-/*   Updated: 2022/01/26 12:16:23 by hanjung          ###   ########.fr       */
+/*   Updated: 2022/01/27 11:20:16 by hanjung          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int	ft_strlen(char *arr)
+int	check_sep(char c, char *charset)
 {
-	int	i;
-
-	i = 0;
-	while (arr[i])
-		i++;
-	return (i);
+	while (*charset)
+	{
+		if (c == *charset)
+			return (1);
+		charset++;
+	}
+	return (0);
 }
 
-void	ft_malloc2(char *str, char **arr, int size)
+int	get_size(char *str, char *charset)
 {
-	int	i;
-	int	str_size;
-
-	str_size = ft_strlen(str);
-	i = -1;
-	while (++i < size + 1)
-		arr[i] = (char *)malloc(sizeof(char) * (str_size + 1));
-}
-
-char	**ft_malloc(char *str, char *charset)
-{
-	int	i;
-	int j;
 	int	size;
-	char **arr;
 
 	size = 0;
-	i = -1;
-	while (str[++i])
+	while (*str)
 	{
-		j = -1;
-		while (charset[++j])
-			if (str[i] == charset[j])
-				size++;
+		if (!check_sep(*str, charset))
+		{
+			size++;
+			while (*str && !check_sep(*str, charset))
+				str++;
+		}
+		str++;
 	}
-	arr = (char **)malloc(sizeof(char) * (size + 2) * (ft_strlen(str) + 1));
-	if (!arr)
-		return (NULL);
-	ft_malloc2(str, arr, size);
-	i = -1;
-	while (++i < size + 1)
-		if (!arr[i])
-			return (NULL);
-	return (arr);
+	return (size);
+}
+
+void	ft_strcpy(char *dest, char *start, char *fin)
+{
+	while (start < fin)
+		*(dest++) = *(start++);
+	*dest = 0;
 }
 
 char	**ft_split(char *str, char *charset)
 {
+	int		size;
 	int		i;
-	int		j;
-	int		result_idx;
 	char	**result;
+	char	*temp;
 
-	result = ft_malloc(str, charset);
-	if (!result)
-		return (NULL);
-	*result = str;
-	i = -1;
-	result_idx = 0;
-	while (str[++i])
+	i = 0;
+	size = get_size(str, charset);
+	result = (char **)malloc(sizeof(char *) * (size + 1));
+	while (*str)
 	{
-		j = -1;
-		while (charset[++j])
+		if (!check_sep(*str, charset))
 		{
-			if (str[i] == charset[j])
-			{
-				*(result[result_idx] + i) = 0;
-				result_idx++;
-				*(result + result_idx) = (str + i + 1);
-			}
+			temp = str;
+			while (*str && !check_sep(*str, charset))
+				str++;
+			result[i] = (char *)malloc(sizeof(char) * (str - temp + 1));
+			ft_strcpy(result[i], temp, str);
+			i++;
 		}
+		str++;
 	}
-	result[result_idx + 1] = 0;
+	result[i] = 0;
 	return (result);
-}
-
-#include <stdio.h>
-int main()
-{
-	int i = 0;
-	for(;i < 5; i++)
-	printf("%s\n",ft_split("hello, world!","oed")[i]);
 }
